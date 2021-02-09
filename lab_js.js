70e59974e1172d2592541528d00bc98e1e40da2b
Abdubossit <78731943+Abdubossit@users.noreply.github.com>
2021-02-09 12:45:47 +0600

    Add files via upload

---
+++ lab_js.js
@@ -0,0 +1,7 @@
+function change(element){
+	var a = element.innnerHTML;
+	switch(a){
+		case "Pro":
+			document.getElementById("img").src = "https://image-us.samsung.com/us/smartphones/galaxy-note10/gallery/aura_glow/D1_AuraGlow_front_w_pen.png?$product-details-jpg$"; 
+	}
+}
